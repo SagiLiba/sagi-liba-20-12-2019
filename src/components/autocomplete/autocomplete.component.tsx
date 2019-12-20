b3ca7afa17209468fde546497e5ff53a55cad754
Sagi <libasagi@gmail.com>
2019-12-20 15:48:27 +0200

    Added autocomplete suggestions

--- src/components/autocomplete/autocomplete.component.tsx
+++ src/components/autocomplete/autocomplete.component.tsx
@@ -5,14 +5,15 @@ import rootStores from '../../stores';
 @observer
 export default class Autocomplete extends React.Component {
   render() {
-    console.log(rootStores.viewStore.showSearchBox);
-
     const autocompleteWidthTransition = rootStores.viewStore.showSearchBox ? 'autocomplete-width-transition' : '';
     const autocompleteFadeinTransition = rootStores.viewStore.showSearchBox ? 'autocomplete-fadein-transition' : '';
 
     return (
       <div className={`autocomplete-container ${autocompleteFadeinTransition}`}>
         <input type='text' className={autocompleteWidthTransition} />
+        <div className={`autocomplete-suggestions ${autocompleteWidthTransition}`}>
+          <div className='suggestion'>Tel Aviv</div>
+        </div>
       </div>
     );
   }
